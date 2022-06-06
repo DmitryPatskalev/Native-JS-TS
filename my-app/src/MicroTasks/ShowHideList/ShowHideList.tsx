da870fd4776843a32c3984273acd7f22f86d4abf
DmitryPatskalev <dimanord@yandex.ru>
2022-06-06 06:30:34 +0300

    commit

--- my-app/src/MicroTasks/ShowHideList/ShowHideList.tsx
+++ my-app/src/MicroTasks/ShowHideList/ShowHideList.tsx
@@ -1,30 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import {v1} from "uuid";
+
 
 type ListPerson = {
 	 value: boolean
 	 setValue: (value: boolean) => void
 }
+type ListPersonType = {
+	 id: string
+	 name: string
+	 age: number
+}
 
-let listPerson = [
-	 {name: 'Dima', age: 37},
-	 {name: 'Tania', age: 31},
-	 {name: 'Danik', age: 7},
+let listPerson: Array<ListPersonType> = [
+	 {id: v1(), name: 'Dima', age: 37},
+	 {id: v1(), name: 'Tania', age: 31},
+	 {id: v1(), name: 'Danik', age: 7},
 ]
 
 export const ShowHideList = (props: ListPerson) => {
+	 const [list, setList] = useState(listPerson)
 	 let result;
 	 if (props.value) {
-			result = listPerson.map((elem, index) => {
+			result = list.map((elem, index) => {
 				 return <ul key={index}>
 						<li>
 							 name:{elem.name}---
 							 age:{elem.age}
+							 <button>x</button>
 						</li>
 				 </ul>
 			})
 	 }
 	 const onClickShow = () => props.setValue(!props.value)
-	 
+
 	 return <div>
 			<button onClick={onClickShow}>{!props.value ? 'Show persons' : 'Hide persons'}</button>
 			{result}
--- my-app/src/stories/Input.stories.tsx
+++ my-app/src/stories/Input.stories.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -28,4 +29,38 @@ export const GetValueClickButton = () => {
 			actual value- {result}
 	 </>
 }
+
+export const ControlledInput = () => {
+	 const [parentValue, setParentValue] = useState('')
+	 const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+			setParentValue(event.currentTarget.value)
+	 }
+
+	 return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckBox = () => {
+	 const [parentValue, setParentValue] = useState(true)
+	 const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+			setParentValue(event.currentTarget.checked)
+	 }
+
+	 return <input type='checkbox' checked={parentValue} onChange={onChange}/>
+}
+
+
+export const ControlledSelect = () => {
+	 const [parentValue, setParentValue] = useState<string | undefined>('3')
+	 const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+			setParentValue(event.currentTarget.value)
+	 }
+
+	 return <select value={parentValue} onChange={onChange}>
+			<option value="none">none</option>
+			<option value="1">Minsk</option>
+			<option value="2">Kiev</option>
+			<option value="3">Lviv</option>
+	 </select>
+}
+
 export const ControlledWithFixedValue = () => <input value={'Dmitry'}/>
