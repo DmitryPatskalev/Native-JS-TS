6595d582b3de7e4178b614874054aaad13f5dba3
DmitryPatskalev <dimanord@yandex.ru>
2022-07-10 09:44:55 +0300

    commit

--- my-app/src/TraningReact/State/State.tsx
+++ my-app/src/TraningReact/State/State.tsx
@@ -4,6 +4,14 @@ const State = () => {
 	 const [name, setName] = useState('prod')
 	 const [inCart, setInCart] = useState(false)
 
+	 const [name1, setName1] = useState('Dima')
+	 const [name2, setName2] = useState('Patskalev')
+	 const [buttonName, buttonSetName] = useState('Danik')
+	 const [button2Name, setButton2Name] = useState('Tania')
+	 const [disabled, setDisabled] = useState(false)
+	 const [buttonBan, setButton] = useState(false)
+	 const [age, setAge] = useState(0)
+
 
 	 const onClickHundler = () => {
 			setInCart(!inCart)
@@ -12,6 +20,20 @@ const State = () => {
 	 const changeHundler = () => {
 			setName('cost')
 	 }
+	 const onClickName = () => {
+			setName1(buttonName)
+	 }
+	 const onClick2Name = () => {
+			setName2(button2Name)
+	 }
+	 const ban = () => {
+			setDisabled(!disabled)
+	 }
+	 const bunButton = () => {
+			setButton(!buttonBan)
+	 }
+
+
 	 return (
 		 <div>
 				<button onClick={onClickHundler}>Click</button>
@@ -19,7 +41,19 @@ const State = () => {
 				<p>{name}</p>
 				<button onClick={changeHundler}>change state</button>
 				<hr/>
-
+				<div>{name1}</div>
+				<div>{name2}</div>
+				<button onClick={onClickName}>Change name</button>
+				<button onClick={onClick2Name}>Change 2 name</button>
+				<hr/>
+				<button disabled={buttonBan} onClick={ban}>Ban</button>
+				<span>{!disabled ? 'User is baned' : 'User is not baned'}</span>
+				<hr/>
+				<button onClick={bunButton}>Ban user</button>
+				<hr/>
+				<button disabled={age === 10} onClick={() => setAge(age + 1)}>Age +</button>
+				<span>{age}</span>
+				<button disabled={age === 0} onClick={() => setAge(age - 1)}>Age -</button>
 		 </div>
 	 );
 };
