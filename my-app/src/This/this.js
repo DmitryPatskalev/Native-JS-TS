e6776fa610f5ffdfec54dbb7180d5a3abe0ccff4
DmitryPatskalev <dimanord@yandex.ru>
2022-07-20 21:07:29 +0300

    commit

---
+++ my-app/src/This/this.js
@@ -0,0 +1,9 @@
+// =========== 1 ===========
+
+// console.log(this === window)
+
+// =========== 2 ===========
+
+let animals = ['cat', 'dog', 'wolf']
+let arr = animals.map(elem => elem + ' is good animals')
+console.log(arr)
